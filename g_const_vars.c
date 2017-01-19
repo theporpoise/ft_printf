306b58fb90978ef5a103bf9664cfc9b6587fa211
Matthew Gould <matt@everettgould.com>
2017-01-19 13:09:18 -0800

    putting globals in their own file

---
+++ g_const_vars.c
@@ -0,0 +1,15 @@
+#include "ft_printf.h"
+
+const char	*len_mod[] = {
+	"hh",
+	"h",
+	"ll",
+	"l",
+	"j",
+	"t",
+	"z"
+};
+
+const char	*g_specifier = "sSpdDioOuUxXcC";
+
+const char	*g_flags = "-+ #0";
