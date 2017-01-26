5c5a1b75503d91babc5270492a98632a612a410f
Matthew Gould <matt@everettgould.com>
2017-01-26 13:36:04 -0800

    new files structure

---
+++ libftprintf/pf_g_const_vars.c
@@ -0,0 +1,37 @@
+#include "libft.h"
+
+const char	*len_mod[] = {
+	"hh",
+	"h",
+	"ll",
+	"l",
+	"j",
+	"t",
+	"z"
+};
+
+const char	*g_specifier = "sSpdDioOuUxXcC";
+
+const char	*g_flags = "-+ #0";
+
+/*
+ *Number Specifiers
+ * D
+ * i
+ * o
+ * O
+ * u
+ * U
+ * s
+ * X
+ *
+ *Character Specifiers
+ * s
+ * S
+ * c
+ * C
+ *
+ * Pointer Specifier
+ * p
+ *
+*/
