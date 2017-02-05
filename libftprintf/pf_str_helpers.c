a4bda3d2781927df1bb19f3eecf8bc02bd44e7f3
Matthew Gould <matt@everettgould.com>
2017-02-05 14:33:11 -0800

    breakdown functions into smaller sub modules to norm

---
+++ libftprintf/pf_str_helpers.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pf_str_helpers.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/05 11:37:15 by mgould            #+#    #+#             */
+/*   Updated: 2017/02/05 14:10:56 by mgould           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include "libft.h"
+#include <limits.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <wchar.h>
+
+char	*str_precision_handler(t_box *box, char *value)
+{
+	int		str_len;
+	char	*ret;
+
+	if (value == NULL || box->precision < 0)
+	{
+		return (value);
+	}
+	str_len = ft_strlen(value);
+	ret = ft_strdup(value);
+	if (str_len > box->precision)
+		ret[box->precision] = '\0';
+	return (ret);
+}
+
+char	*str_printer(t_box *box, va_list *param_list, char *value)
+{
+	value = (char *)(wint_t *)va_arg(*param_list, wint_t *);
+	// this is failing ftprintf.com, return something other than NULLc
+	if (value == NULL)
+		return (value);
+	else
+	{
+		value = str_precision_handler(box, value);
+		value = str_field_width_handler(box, value);
+		if (box->minus_flag > 0)
+			left_align_str(value);
+	}
+	return (value);
+}
