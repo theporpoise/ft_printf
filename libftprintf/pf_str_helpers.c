098de8299babd5d8cc6c167fb1752c68cabb3a90
Matthew Gould <matt@everettgould.com>
2017-02-07 14:13:16 -0800

    update for chars works

--- libftprintf/pf_str_helpers.c
+++ libftprintf/pf_str_helpers.c
@@ -6,7 +6,7 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/05 11:37:15 by mgould            #+#    #+#             */
-/*   Updated: 2017/02/07 12:50:13 by mgould           ###   ########.fr       */
+/*   Updated: 2017/02/07 14:10:37 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,9 +57,15 @@ char	*str_printer(t_box *box, va_list *param_list, char *value)
 	}
 	else if (box->specifier == 'S')
 	{
+
 		value = (char *)(wchar_t *)va_arg(*param_list, wchar_t *);
 		if (value == NULL)
 			return (value);
+		value = ft_strdup(value);
+		value = str_precision_handler(box, value);
+		value = str_field_width_handler(box, value);
+		if (box->minus_flag > 0)
+			left_align_str(value);
 	}
 	return (value);
 }
